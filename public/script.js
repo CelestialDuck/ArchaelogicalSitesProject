9daa9ea86b7ed680f9c0ee18aaceb50e9aa64333
CelestialDuck <thecelestialsprison@gmail.com>
2022-08-16 22:04:24 -0400

    Drop down list is sorted

--- public/script.js
+++ public/script.js
@@ -1,17 +1,23 @@
 
-console.log("Anything runnig here?");
+//LEAFLET MAP SET UP//
 const myMap = L.map("map").setView([29,31], 6);
 
-      		
+
 const attribution = "&copy; <a href='https://www.openstreetmap.org/copyright'>OpenStreetMap</a>"
 const tile_url = "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png";
 const tiles = L.tileLayer(tile_url, { attribution }); 
 tiles.addTo(myMap);
 //const api_url = "https://api.wheretheiss.at/v1/satellites/25544";
 
+
 const interaction_div_dynasty = document.getElementById("dynasty");
 const interaction_div_pharaoh = document.getElementById("pharaoh");
 
+const pharaohList = [];
+const dynastyList = [];
+
+var currentDynasty;
+var currentPharaoh;
 const pyramid_icon = L.icon({
 	iconUrl: 'map_icons/pyramid_icon.png',
 	iconSize: [20, 20],
@@ -19,24 +25,14 @@ const pyramid_icon = L.icon({
 });
 
 getmap();
-
+getddl();
             
 async function getmap(){
         const response = await fetch('/api');
                 
         const data = await response.json();
        	for (item of data){
-       	
        		//variables in use
-       		let dynastyDuplicate = false;
-       		let pharaohDuplicate = false;
-   
-       		const dynastyList = document.querySelectorAll(".dynasty_ddl");
-       		const pharaohList = document.querySelectorAll(".pharaoh_dll");
-       		
-       		console.log("How many times does this loop?");
-       		
-       		
        		const tooltip_string = "<h2>"+
        		item.modernname+
        		"</h2> <p> Ancient Name: "+
@@ -49,7 +45,16 @@ async function getmap(){
        		item.location+
        		"</p>";
        		
+       		if(!dynastyList.includes(item.dynasty)){
+       			dynastyList.push(item.dynasty);
+       		} 
        		
+       		if(!pharaohList.includes(item.pharaoh)){
+       			pharaohList.push(item.pharaoh);
+       		}
+       		
+       			
+       		//Add each database entry to the map div//
        		const marker = L.marker(
        			[item.latitude, item.longitude], 	//LAT + LON
        			{icon: pyramid_icon}).bindTooltip(	//ICON
@@ -57,23 +62,80 @@ async function getmap(){
        					{direction: 'auto', 		//
        					permanent: false, 		//
        					sticky: false}).addTo(myMap);	//
+       	}
+        
+        //marker.setLatLng([0, 0]); //TAKES LATITUDE AND LONGITUDE
+        //document.getElementById("lat").textContent = latitude;
+        //document.getElementById("lon").textContent = longitude;
+	//console.log(data);
+	dynastyList.sort(function(a,b){return a - b});
+	pharaohList.sort();
+	//Populate the drop down list here:
+	dynastyList.forEach(function(item) {
+		const dynastyOption = document.createElement("option");
+		dynastyOption.textContent = item;
+		//dynastyOption.setAttribute("class", "dynasty_ddl") //Class element used for finding unique values in the list
+		dynastyOption.setAttribute("value", item);
+		interaction_div_dynasty.append(dynastyOption);
+	});
+	
+	pharaohList.forEach(function(item) {
+		const pharaohOption = document.createElement("option");
+		pharaohOption.textContent = item;
+		pharaohOption.setAttribute("value", item);
+		interaction_div_pharaoh.append(pharaohOption);
+	});
+}
+
+function getddl(){
+	console.log(dynastyList);
+	
+	//CHECKING FOR DUPLICATES//
+       		/*console.log("Dynasties:");
+       		console.log(dynastyList);
+       		console.log("Pharaohs:");
+       		console.log(pharaohList);*/ //test code
        		
-       		//CHECK FOR DUPLICATES
+       		
+       		
+       		//Check dynasty:
+       		/*if(dynastyList.length == 0){
+       			console.log("It gets here right?");
+       			dynastyDuplicate = true;
+       			const dynastyOption = document.createElement("option");
+			dynastyOption.textContent = item.dynasty;
+			dynastyOption.setAttribute("class", "dynasty_ddl") //Class element used for finding unique values in the list
+			dynastyOption.setAttribute("value", item.dynasty);
+			interaction_div_dynasty.append(dynastyOption);
+       		} else {
        		dynastyList.forEach(function(dynasty) {
-       			if(dynasty.getAttribute("value") == item.dynasty){
+       			if(dynasty.getAttribute("value") === item.dynasty){
        				dynastyDuplicate = true;
        			}
        		});
+       		}
        		
-		
+		//Check pharoah:
+		if(pharaohList.length == 0){
+			//console.log("What about here?");
+			pharoahDuplucate = true; //avoids the next if for first entry
+			const pharaohOption = document.createElement("option");
+			pharaohOption.textContent = item.pharaoh;
+			pharaohOption.setAttribute("class", "pharaoh_ddl"); //Class element used for finding unique values in the list
+			pharaohOption.setAttribute("value", item.pharaoh);
+			interaction_div_pharaoh.append(pharaohOption);	
+		} else {
 		pharaohList.forEach(function(pharaoh) {
-			if(pharaoh.getAttribute("value") == item.pharaoh){
+			const pharaohname = pharaoh.getAttribute("value");
+			//console.log(pharaohname); //TESSST CODE
+			if((pharaohname === item.pharaoh) || (pharaohname.includes(item.pharaoh))){
 				pharaohDuplicate = true;
+				//console.log("Is this turning out true?");
 			}
 		});
-		
+		}
 		//ADD OPTIONS IF NOT DUPLICATE
-		if(dynastyDuplicate == false){
+		if(dynastyDuplicate === false){
 			const dynastyOption = document.createElement("option");
 			dynastyOption.textContent = item.dynasty;
 			dynastyOption.setAttribute("class", "dynasty_ddl") //Class element used for finding unique values in the list
@@ -81,18 +143,12 @@ async function getmap(){
 			interaction_div_dynasty.append(dynastyOption);
 		}
 		
-		if(pharaohDuplicate == false){
+		if(pharaohDuplicate === false){
 			const pharaohOption = document.createElement("option");
 			pharaohOption.textContent = item.pharaoh;
-			pharaohOption.setAttribute("class", "phaoroah_dll"); //Class element used for finding unique values in the list
+			pharaohOption.setAttribute("class", "pharaoh_ddl"); //Class element used for finding unique values in the list
 			pharaohOption.setAttribute("value", item.pharaoh);
 			interaction_div_pharaoh.append(pharaohOption);
-		}
-       	}
-        
-        //marker.setLatLng([0, 0]); //TAKES LATITUDE AND LONGITUDE
-        //document.getElementById("lat").textContent = latitude;
-        //document.getElementById("lon").textContent = longitude;
-	//console.log(data);
+		}*/
 }
 

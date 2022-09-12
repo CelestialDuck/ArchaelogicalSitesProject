2a11c992418732c0f44e288c155db272edbac397
CelestialDuck <thecelestialsprison@gmail.com>
2022-09-12 05:07:54 -0400

    Added CSS loading screen

--- public/script.js
+++ public/script.js
@@ -22,6 +22,8 @@ const information_div_sts = document.getElementById("information_stats");
 const pharaohList = [];
 const dynastyList = [];
 
+const main = document.querySelector(".mainpage");
+const load = document.querySelector(".loadpage");
 
 const pyramid_icon = L.icon({
 	iconUrl: 'map_icons/pyramid_icon.png',
@@ -29,12 +31,29 @@ const pyramid_icon = L.icon({
 	iconAnchor: [10,10]
 });
 
+function init(){
+	setTimeout(() => {
+		load.style.opacity = 0;
+		load.style.display = "none";
+		
+		
+		main.style.display = "block";
+		setTimeout(() => {
+			main.style.opacity = 1
+			}, 50);
+		}, 3000);
+	myMap.invalidateSize();
+}
 
+//Starts the application
 getmap();
-getddl();
+init();
+
 
 interaction_div_dynasty.addEventListener("change", dynastyChange);
 interaction_div_pharaoh.addEventListener("change", pharaohChange);
+
+
 
 async function getmap(){
         const response = await fetch('/api');
@@ -254,68 +273,5 @@ async function pharaohChange(){
 function resetddl(select_option){
 	select_option.selectedIndex = 0;
 	return;
-}
-
-function getddl(){
-	//CHECKING FOR DUPLICATES//
-       		/*console.log("Dynasties:");
-       		console.log(dynastyList);
-       		console.log("Pharaohs:");
-       		console.log(pharaohList);*/ //test code
-       		
-       		
-       		
-       		//Check dynasty:
-       		/*if(dynastyList.length == 0){
-       			console.log("It gets here right?");
-       			dynastyDuplicate = true;
-       			const dynastyOption = document.createElement("option");
-			dynastyOption.textContent = item.dynasty;
-			dynastyOption.setAttribute("class", "dynasty_ddl") //Class element used for finding unique values in the list
-			dynastyOption.setAttribute("value", item.dynasty);
-			interaction_div_dynasty.append(dynastyOption);
-       		} else {
-       		dynastyList.forEach(function(dynasty) {
-       			if(dynasty.getAttribute("value") === item.dynasty){
-       				dynastyDuplicate = true;
-       			}
-       		});
-       		}
-       		
-		//Check pharoah:
-		if(pharaohList.length == 0){
-			//console.log("What about here?");
-			pharoahDuplucate = true; //avoids the next if for first entry
-			const pharaohOption = document.createElement("option");
-			pharaohOption.textContent = item.pharaoh;
-			pharaohOption.setAttribute("class", "pharaoh_ddl"); //Class element used for finding unique values in the list
-			pharaohOption.setAttribute("value", item.pharaoh);
-			interaction_div_pharaoh.append(pharaohOption);	
-		} else {
-		pharaohList.forEach(function(pharaoh) {
-			const pharaohname = pharaoh.getAttribute("value");
-			//console.log(pharaohname); //TESSST CODE
-			if((pharaohname === item.pharaoh) || (pharaohname.includes(item.pharaoh))){
-				pharaohDuplicate = true;
-				//console.log("Is this turning out true?");
-			}
-		});
-		}
-		//ADD OPTIONS IF NOT DUPLICATE
-		if(dynastyDuplicate === false){
-			const dynastyOption = document.createElement("option");
-			dynastyOption.textContent = item.dynasty;
-			dynastyOption.setAttribute("class", "dynasty_ddl") //Class element used for finding unique values in the list
-			dynastyOption.setAttribute("value", item.dynasty);
-			interaction_div_dynasty.append(dynastyOption);
-		}
-		
-		if(pharaohDuplicate === false){
-			const pharaohOption = document.createElement("option");
-			pharaohOption.textContent = item.pharaoh;
-			pharaohOption.setAttribute("class", "pharaoh_ddl"); //Class element used for finding unique values in the list
-			pharaohOption.setAttribute("value", item.pharaoh);
-			interaction_div_pharaoh.append(pharaohOption);
-		}*/
 }
 
